a68b9d5fc31974322e83b2aea924da8bcedcd5bd
wizAmit <amitforfriends_dns@yahoo.com>
2024-11-18 11:16:49 -0500

    echo working

--- app/src/main/java/io/jespen/lib/handlers/MessageHandler.java
+++ app/src/main/java/io/jespen/lib/handlers/MessageHandler.java
@@ -3,6 +3,7 @@ package io.jespen.lib.handlers;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
+import io.jespen.lib.Headers;
 import io.jespen.lib.InitReqPd;
 import io.jespen.lib.Message;
 import io.jespen.lib.ResBuilder;
@@ -11,8 +12,12 @@ public interface MessageHandler {
 
     static final AtomicInteger msgId = new AtomicInteger();
 
-    public Function<Message, Message> init = Function.identity();
+    Function<Message, Headers> reverseHeaders = (Message msg) -> {
+        return new Headers(msg.headers().dest(), msg.headers().src());
+    };
 
-    public Message handle(Message req);
+//    public Function<Message, Message> init = Function.identity();
+
+//    public Message handle(Message req);
 
 }
